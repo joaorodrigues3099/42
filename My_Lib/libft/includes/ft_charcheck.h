0ad9e73b24e557e4f568ec9a362ef317500af2a0
zacka69 <joaorodrigues3099@gmail.com>
2024-11-09 01:11:43 +0100

    added My_Lib

---
+++ My_Lib/libft/includes/ft_charcheck.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   charcheck.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joao-alm <support@toujoustudios.net>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 22:44:41 by joao-alm          #+#    #+#             */
+/*   Updated: 2024/11/08 22:45:42 by joao-alm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHAR_CHECK_H
+#define CHAR_CHECK_H
+
+int					ft_isalpha(int c);
+int					ft_isdigit(int c);
+int					ft_isalnum(int c);
+int					ft_isascii(int c);
+int					ft_isprint(int c);
+int					ft_toupper(int c);
+int					ft_tolower(int c);
+
+#endif //CHAR_CHECK_H
