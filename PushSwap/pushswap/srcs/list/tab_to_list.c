20c2a220e11b5f0e411c23dac8a99bcf68da9af1
zacka69 <joaorodrigues3099@gmail.com>
2024-11-18 19:19:10 +0100

    libft version

---
+++ PushSwap/pushswap/srcs/list/tab_to_list.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tab_to_list.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joao-alm <joao-alm@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 09:08:58 by joao-alm          #+#    #+#             */
+/*   Updated: 2024/11/18 09:08:58 by joao-alm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+#include "../libft/includes/linklist.h"
+
+t_list	*ft_get_lst(int **tab, int size)
+{
+	t_list	*lst;
+	int		i;
+
+	lst = NULL;
+	i = 0;
+	while (i < size)
+		
+}
--- PushSwap/pushswap/srcs/ranges.c
+++ PushSwap/pushswap/srcs/ranges.c
@@ -41,53 +41,35 @@ void	ft_sort_tab(int *tab, int size)
 	}
 }
 
-int	**ft_alloc_quartiles(int **quartiles)
+t_range *ft_fill_quartiles(t_range	*ranges, int *tab, int size)
 {
-	int	i;
+	ranges[0].min = INT_MIN;
+	ranges[0].max = tab[size / 4];
+	ranges[1].min = tab[size / 4];
+	ranges[1].max = tab[size / 2];
+	ranges[2].min = tab[size / 2];
+	ranges[2].max = tab[3 * (size / 4)];
+	ranges[3].min = tab[3 * (size / 4)];
+	ranges[3].max = INT_MAX;
+	return (ranges);
+}
 
-	quartiles = (int **)malloc(4 * sizeof(int *));
-	if (!quartiles)
+t_range	*ft_get_quartiles(int *tab, int size)
+{
+	int		*sorted_tab;
+	int		i;
+	t_range	*ranges;
+
+	ranges = (t_range *)malloc(4 * sizeof(t_range));
+	if (!ranges)
 		return (NULL);
-	i = -1;
-	while (++i < 4)
-	{
-		quartiles[i] = (int *)malloc(2 * sizeof(int));
-		if (!quartiles[i])
-			return (ft_free_matrix((void **)quartiles, i), NULL);
-	}
-	return (quartiles);
-}
-
-int	**ft_fill_quartiles(int **quartiles, int *tab, int size)
-{
-	quartiles[0][0] = INT_MIN;
-	quartiles[0][1] = tab[size / 4];
-	quartiles[1][0] = tab[size / 4];
-	quartiles[1][1] = tab[size / 2];
-	quartiles[2][0] = tab[size / 2];
-	quartiles[2][1] = tab[3 * (size / 4)];
-	quartiles[3][0] = tab[3 * (size / 4)];
-	quartiles[3][1] = INT_MAX;
-	return (quartiles);
-}
-
-int	**ft_get_quartiles(int *tab, int size)
-{
-	int	**quartiles;
-	int	*sorted_tab;
-	int	i;
-
-	quartiles = NULL;
 	sorted_tab = (int *)malloc(size * sizeof(int));
 	if (!sorted_tab)
-		return (NULL);
-	quartiles = ft_alloc_quartiles(quartiles);
-	if (!quartiles)
-		return (NULL);
+		return (free(ranges), NULL);
 	i = -1;
 	while (++i < size)
 		sorted_tab[i] = tab[i];
 	ft_sort_tab(sorted_tab, size);
-	return (ft_fill_quartiles(quartiles, tab, size));
+	return (ft_fill_quartiles(ranges, tab, size));
 }
 
