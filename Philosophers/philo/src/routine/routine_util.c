89e16437d0267165721c9295105972fb6887e51a
zacka69 <joaorodrigues3099@gmail.com>
2025-02-07 18:12:08 +0100

    Philosophers working

---
+++ Philosophers/philo/src/routine/routine_util.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine_util.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joao-alm <joao-alm@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/07 14:43:47 by joao-alm          #+#    #+#             */
+/*   Updated: 2025/02/07 14:45:32 by joao-alm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+#include "colour.h"
+#include <stdio.h>
+
+int	ft_print_status(t_philo *philo, const char *msg)
+{
+	if (ft_stop_routine(philo))
+		return (-1);
+	pthread_mutex_lock(&philo->data->print);
+	printf("[%ld] " BOLD "%d " RESET "%s\n", ft_time_ms()
+		- philo->data->start_time, philo->id, msg);
+	pthread_mutex_unlock(&philo->data->print);
+	return (0);
+}
+
+int	ft_stop_routine(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->data->monitor);
+	if (philo->data->death || philo->data->n_full == philo->data->n_philos)
+	{
+		pthread_mutex_unlock(&philo->data->monitor);
+		return (-1);
+	}
+	pthread_mutex_unlock(&philo->data->monitor);
+	return (0);
+}
---
+++ Philosophers/philo/src/util/error.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_error.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joao-alm <joao-alm@student.42luxembourg.>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/02 17:01:30 by joao-alm          #+#    #+#             */
+/*   Updated: 2025/02/02 13:32:57 by joao-alm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "colour.h"
+#include "error.h"
+#include <unistd.h>
+
+static void	ft_puterror(const char *error)
+{
+	write(2, "Error\n", 6);
+	write(2, PINK, 24);
+	while (*error)
+		write(2, error++, 1);
+	write(2, RESET, 4);
+}
+
+static void	ft_standard_error(const int errno)
+{
+	if (errno == E_FORMAT)
+		ft_puterror("Format: ./philo <n_philos> <die_time> <eat_time> "
+			"<sleep_time> [n_meals]\n");
+	else if (errno == E_MEM_ALLOC)
+		ft_puterror("Memory allocation failed\n");
+	else if (errno == E_CREATE_THREAD)
+		ft_puterror("Failed to create thread\n");
+	else if (errno == E_JOIN_THREAD)
+		ft_puterror("Failed to join thread\n");
+	else if (errno == E_MUTEX_INIT)
+		ft_puterror("Failed to initiate mutex\n");
+}
+
+int	ft_print_error(const int errno)
+{
+	if (errno < 100)
+		ft_standard_error(errno);
+	else if (errno == E_PHILO)
+		ft_puterror("<n_philos> must be a value between 1 and 200\n");
+	else if (errno == E_TIMES)
+		ft_puterror("<die_time>, <eat_time> and <sleep_time> must be a value "
+			"between 1 and INT_MAX\n");
+	else if (errno == E_MEALS)
+		ft_puterror("[n_meals] must be a value between 0 and INT_MAX\n");
+	return (errno);
+}
